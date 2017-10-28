8fe2e40b68d063adfa1c9305d3eab35639367f31
Michael Sweetland <mjsweet@umich.edu>
2017-10-28 15:06:35 -0500

    current

--- arch-hacks-react-client/src/containers/Patient/PatientHome.js
+++ arch-hacks-react-client/src/containers/Patient/PatientHome.js
@@ -2,11 +2,14 @@ import React, { Component } from "react";
 //import "./Home.css";
 import Login from "../Login";
 
+import LineChart from 'react-linechart';
+import '../../../node_modules/react-linechart/dist/styles.css';
+
 export default class PatientHome extends Component {
 
   constructor(props) {
       super(props);
-      this.state = {loggedIn:false};
+      this.state = {loggedIn:false, };
 
       this.handleLoggedIn = this.handleLoggedIn.bind(this);
   }
@@ -18,11 +21,22 @@ export default class PatientHome extends Component {
 
 
   render() {
+    const data = [
+            {
+                color: "black",
+                points: [{x: 1, y: 2}, {x: 3, y: 5}, {x: 7, y: -3}]
+            }
+        ];
+
     return (
       <div>
         {this.state.loggedIn ?
           <div>
-            patient dashboard
+            <LineChart
+                        width={600}
+                        height={400}
+                        data={data}
+                    />
 
 
 
